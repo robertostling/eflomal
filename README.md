dc2ff841cab9c315d12ad385b4523c24bd148a8c
miau1 <aulamo.mikko@gmail.com>
2022-11-11 18:04:37 +0200

    pip installation

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# pip installable eflomal
+
+This is a pip installable `eflomal` package for the python scripts `align.py`, `makepriors.py` and `mergefiles.py`. Installation:
+
+   `pip install eflomal`
+
 # eflomal
 Efficient Low-Memory Aligner
 
--- setup.py
+++ setup.py
@@ -7,13 +7,20 @@ import numpy
 cyalign_ext=Extension('eflomal', ['python/eflomal/eflomal.pyx'],
                       include_dirs=[numpy.get_include()])
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='eflomal',
     version='0.1',
     author='Robert Östling',
     url='https://github.com/robertostling/eflomal',
     license='GNU GPLv3',
-    install_requires=['numpy'],
-    ext_modules=cythonize(cyalign_ext)
+    description='pip installable eflomal',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['numpy', 'Cython'],
+    ext_modules=cythonize(cyalign_ext, language_level='3'),
+    scripts=['align.py', 'makepriors.py', 'mergefiles.py']
 )
 
